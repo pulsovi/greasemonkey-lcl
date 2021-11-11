b63b246e9e6afb241ebe60a1a00f789605eb7017
David GABISON <david.gabison@outlook.com>
2021-11-11 17:43:12 +0100

    fix: fix get one mail after modifications of outlook.com front code

--- Outlook get one mail.js
+++ Outlook get one mail.js
@@ -8,15 +8,12 @@
 /// GLOBALES
 // selecteur pour un element dont la presence atteste qu'un mail est ouvert en lecture
 const mailOpened = '[aria-label="Corps du message"]';
-const globalMailContainer = '[aria-label="Volet de lecture"]'
-
+const refreshInterval = 200;
 /// GLOBALES
 
-console.log("refresh start");
 const { document } = unsafeWindow;
-setInterval(refresh, 200);
-
 const logo = document.body.appendChild(document.createElement('a'));
+
 logo.style.display = 'block';
 logo.style.width = '10px';
 logo.style.height = '10px';
@@ -29,33 +26,13 @@ logo.className = 'greasemonkey-one-mail';
 setInterval(() => {
   const contenu = document.querySelector(mailOpened);
   logo.style.background = contenu ? 'green' : 'red';
-}, 200);
+}, refreshInterval);
 
-function refresh () {
-  let mailId = 'unknown';
+setInterval(() => {
   const contenu = document.querySelector(mailOpened);
   if (!contenu) return;
-  const instance = contenu[Object.keys(contenu).find(key => key.startsWith('__reactInternalInstance'))];
-  if (!instance) return;
-  const jobId = { finish: false };
-  race(instance, jobId, 'root').then(id => { mailId = id; });
+  const mailId = Object.entries(document.querySelector('._2qM5JFJlSDFbw17aZw2PSp'))
+    .find(([key]) => key.startsWith('__reactInternalInstance$'))[1]
+    .memoizedProps.children[0].props.itemId.Id;
   logo.href = `https://outlook.live.com/mail/0/deeplink/read/${encodeURIComponent(mailId)}`;
-}
-
-async function race (instance, jobId) {
-  await sleep(0);
-  if (jobId.finish) return null;
-  const keys = Object.keys(instance);
-  if (keys.includes('firstItemId')) {
-    jobId.finish = true;
-    return instance.firstItemId;
-  }
-  return Promise.race(keys
-    .filter(key => typeof key === 'number' ||
-      'props,child,pendingProps,children,viewState'.split(',').includes(key))
-    .map(key => race(instance[key], jobId)));
-}
-
-function sleep (ms) {
-  return new Promise(rs => setTimeout(rs, ms));
-}
+}, refreshInterval);
